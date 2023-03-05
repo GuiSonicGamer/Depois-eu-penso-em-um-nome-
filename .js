5d532aa9590a3aa80f291890d9f8daf2fa201b9a
GuiSonicGamer <126628777+GuiSonicGamer@users.noreply.github.com>
2023-03-05 18:57:28 -0300

    Add files via upload

---
+++ .js
@@ -0,0 +1,13 @@
+function maisSUS()
+{ document.getElementById("imagem").src="https://pbs.twimg.com/media/FFyzipNXIAoyXQO.jpg";
+}
+function SUS()
+{ document.getElementById("imagem").src="https://memetemplate.in/uploads/1643224858.jpeg";
+}
+function telaSUS()
+{
+document.getElementById("bodySUS").style.backgroundColor="black";
+document.getElementById("titulo").style.color="blue";
+document.getElementById("pSUS").style.color="blue";
+document.getElementById("botaoSUS").style.color="blue"
+}
